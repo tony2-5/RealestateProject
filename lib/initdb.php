b39c58ded7a686dada8e3287f3c926d9312f0eef
tony2-5 <antdvory@gmail.com>
2024-04-22 13:31:24 -0400

    Initial commit with basic database setup

---
+++ lib/initdb.php
@@ -0,0 +1,15 @@
+<?php
+
+$env = parse_ini_file('.env');
+$db_pass = $env["DB_PASS"];
+$db_user="ajd99";
+$db_server="sql2.njit.edu";
+$db_name="ajd99";
+$connection="";
+try{
+  $connection= mysqli_connect($db_server,$db_user,$db_pass,$db_name);
+}catch(mysqli_sql_exception) {
+  echo "Could not connect";
+}
+
+?>
---
+++ visitScheduling/testdb.php
@@ -0,0 +1,12 @@
+<?php
+
+  require("../lib/initdb.php");
+
+  $query = "SELECT * from COMPANY";
+  $result = mysqli_query($connection,$query);
+  if(mysqli_num_rows($result)>0) {
+    $row=mysqli_fetch_assoc($result);
+    echo $row["Name"];
+  }
+  mysqli_close($connection);
+?>
