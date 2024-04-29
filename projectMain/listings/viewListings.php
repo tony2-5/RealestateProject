5a607bc5530517cf10949bdbdd99099e3f5f1abd
mythreya75 <mythreyav09@gmail.com>
2024-04-29 15:45:18 -0400

    Created table for listing

---
+++ projectMain/listings/viewListings.php
@@ -0,0 +1,16 @@
+<?php
+// using sessions to transfer sensitive data between pages
+require_once('../../lib/nav.php');
+?>
+<link rel="stylesheet" href="./style.css">
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Date</th>
+      <th scope="col">Time</th>
+      <th scope="col">Name</th>
+      <th scope="col">Address</th>
+    </tr>
+  </thead>
+  
+</table>
